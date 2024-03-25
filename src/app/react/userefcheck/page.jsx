30280d361ed569554871f1f12ea5badfb49debde
YukoKano <halloween_1031@hotmail.co.jp>
2024-03-26 00:24:02 +0900

    ちょっと追加

--- src/app/react/userefcheck/page.jsx
+++ src/app/react/userefcheck/page.jsx
@@ -6,6 +6,14 @@ import { UseRefItemImage } from "@/app/_components/UseRefItem/UseRefItemImage";
 export default function userefcheck() {
   return (
     <>
+      <p>これを試したよ</p>
+      <a
+        href="https://ja.react.dev/reference/react/useRef"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        https://ja.react.dev/reference/react/useRef
+      </a>
       <UseRefItem />
       <UseRefItemDom />
       <UseRefItemImage />

a1a43d1ed5b4e895253322bdc5c80aaaf1f1e974
YukoKano <halloween_1031@hotmail.co.jp>
2024-03-26 23:32:12 +0900

    useEffectの例を作ってみる

---
+++ src/app/_components/UseEffectItem/ChatRoomConnection.jsx
@@ -0,0 +1,68 @@
+"use client"; // やっぱこれいるっぽい
+
+import { useEffect, useState } from "react";
+
+const createConnection = (serverUrl, roomId) => {
+  return {
+    connect() {
+      console.log(
+        '✅ Connecting to "' + roomId + '" room at ' + serverUrl + "..."
+      );
+    },
+    disconnect() {
+      console.log('❌ Disconnected from "' + roomId + '" room at ' + serverUrl);
+    },
+  };
+};
+
+const ChatRoom = ({ roomId }) => {
+  const [serverUrl, setServerUrl] = useState("https://localhost:1234");
+
+  useEffect(() => {
+    const connection = createConnection(serverUrl, roomId);
+    connection.connect(); // setup
+    return () => {
+      connection.disconnect(); // cleanup
+    };
+  }, [serverUrl, roomId]);
+
+  return (
+    <>
+      <label>
+        serverURL: {""}
+        <input
+          value={serverUrl}
+          onChange={(e) => setServerUrl(e.target.value)}
+        />
+      </label>
+      <h3>welcome to {roomId} room!</h3>
+    </>
+  );
+};
+
+export const ChatRoomConnection = () => {
+  const [roomId, setRoomId] = useState("general");
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <label>
+        choose the chat room : {""}
+        <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+          <option value="general">General</option>
+          <option value="travel">Travel</option>
+          <option value="music">Music</option>
+        </select>
+      </label>
+      <button
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        {show ? "close chat" : "open chat"}
+      </button>
+      {show && <hr />}
+      {show && <ChatRoom roomId={roomId} />}
+    </>
+  );
+};
---
+++ src/app/react/useeffectcheck/page.jsx
@@ -0,0 +1,18 @@
+import { ChatRoomConnection } from "@/app/_components/UseEffectItem";
+
+export default function useeffectcheck() {
+  return (
+    <>
+      <h2>useEffect使うよ</h2>
+      <p>これを試したよ</p>
+      <a
+        href="https://ja.react.dev/reference/react/useEffect"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        https://ja.react.dev/reference/react/useEffect
+      </a>
+      <ChatRoomConnection />
+    </>
+  );
+}

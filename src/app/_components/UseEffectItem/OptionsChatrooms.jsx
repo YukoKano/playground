afa9058ad06cd9a351acb65d8f8a495eb7e14763
YukoKano <halloween_1031@hotmail.co.jp>
2024-04-17 00:01:57 +0900

    コンポーネントで再レンダーされるたびに生成されてしまうオブジェクト・レンダーごとに異なってしまう関数は依存配列にいれず、useEffect内で宣言する

---
+++ src/app/_components/UseEffectItem/OptionsChatrooms.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import { createConnection } from "./chat.js";
+
+const serverUrl = "https://localhost:1234";
+
+function ChatRoom({ roomId }) {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // コンポーネント再レンダーのたびにオブジェクト再生成してしまうので、optionsをuseEffectの中に入れる
+    // const options = {
+    //   serverUrl: serverUrl,
+    //   roomId: roomId,
+    // };
+
+    // ↓がレンダーごとに異なるため、毎回再接続してしまう。関数もuseEffectの中に入れる
+    function createOptions() {
+      return {
+        serverUrl: serverUrl,
+        roomId: roomId,
+      };
+    }
+
+    const options = createOptions();
+    const connection = createConnection(options);
+    connection.connect();
+    return () => connection.disconnect();
+  }, [roomId]);
+
+  return (
+    <>
+      <h1>Welcome to the {roomId} room!</h1>
+      <input value={message} onChange={(e) => setMessage(e.target.value)} />
+    </>
+  );
+}
+
+export default function App() {
+  const [roomId, setRoomId] = useState("general");
+  return (
+    <>
+      <label>
+        Choose the chat room:{" "}
+        <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+          <option value="general">general</option>
+          <option value="travel">travel</option>
+          <option value="music">music</option>
+        </select>
+      </label>
+      <hr />
+      <ChatRoom roomId={roomId} />
+    </>
+  );
+}
